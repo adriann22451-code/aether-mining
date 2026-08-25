2f858a2fe76676da673d74275ef81e751f0f08d4
adriann22451-code <adriann22451@gmail.com>
2026-08-25 17:27:38 +0700

    Add files via upload

--- src/data/missions.js
+++ src/data/missions.js
@@ -1,6 +1,6 @@
 export const missionCatalog = [
-  { id: 1, title: "Claim AETHER 3x today", total: 3, reward: 150, getProgress: (s) => s.dailyClaims },
-  { id: 2, title: "Upgrade 1 Rig Part", total: 1, reward: 100, getProgress: (s) => s.upgradeCount },
-  { id: 3, title: "Reach 2 GH/s Hashrate", total: 2e9, reward: 500, getProgress: (s) => s.totalHashrate },
-  { id: 4, title: "Visit the Marketplace", total: 1, reward: 80, getProgress: (s) => (s.marketVisited ? 1 : 0) },
+  { id: 1, title: "Claim AETHER 3x today", total: 3, reward: 110, getProgress: (s) => s.dailyClaims },
+  { id: 2, title: "Upgrade 1 Rig Part", total: 1, reward: 75, getProgress: (s) => s.upgradeCount },
+  { id: 3, title: "Reach 2 GH/s Hashrate", total: 2e9, reward: 380, getProgress: (s) => s.totalHashrate },
+  { id: 4, title: "Visit the Marketplace", total: 1, reward: 60, getProgress: (s) => (s.marketVisited ? 1 : 0) },
 ];
