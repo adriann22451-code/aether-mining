8aaef1a7176e450c18cd05c9d9769c7ec6a9a33f
adriann22451-code <adriann22451@gmail.com>
2026-08-31 08:05:51 +0700

    Add files via upload

--- src/data/events.js
+++ src/data/events.js
@@ -1,5 +1,5 @@
 export const eventCatalog = [
-  { id: 1, title: "Claim 5x in a row", total: 5, reward: 600, getProgress: (s) => s.claimCount },
-  { id: 2, title: "Collect 50,000 AETHER (lifetime)", total: 50000, reward: 1100, getProgress: (s) => s.totalEarned },
-  { id: 3, title: "Reach 3 GH/s Hashrate", total: 3e9, reward: 1900, getProgress: (s) => s.totalHashrate },
+  { id: 1, title: "Claim 5x in a row", total: 5, reward: 10, getProgress: (s) => s.claimCount },
+  { id: 2, title: "Collect 50,000 AETHER (lifetime)", total: 50000, reward: 100, getProgress: (s) => s.totalEarned },
+  { id: 3, title: "Reach 3 GH/s Hashrate", total: 3e9, reward: 200, getProgress: (s) => s.totalHashrate },
 ];
